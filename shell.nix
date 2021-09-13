45ce709795585e6a5745b544b17c1dc682782987
Gustavo Konrad <g.konrad@outlook.com>
2021-09-13 18:18:59 -0300

    starter

---
+++ shell.nix
@@ -0,0 +1,25 @@
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/21.05.tar.gz";
+  }) {};
+
+  # To update to a newer version of easy-purescript-nix, run:
+  # nix-prefetch-git https://github.com/justinwoo/easy-purescript-nix
+  #
+  # Then, copy the resulting rev and sha256 here.
+  pursPkgs = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "eb64583e3e15749b3ae56573b2aebbaa9cbab4eb";
+    sha256 = "0hr7smk7avdgc5nm1r3drq91j1hf8wimp7sg747832345c8vq19a";
+  }) { inherit pkgs; };
+
+in pkgs.stdenv.mkDerivation {
+  name = "purs-parcel-starter";
+  buildInputs = with pursPkgs; [
+    pursPkgs.purs
+    pursPkgs.spago
+    pursPkgs.zephyr
+    pkgs.nodejs-14_x
+  ];
+}
