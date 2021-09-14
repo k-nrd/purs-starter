c17b5e780f2addd552084955ed5deb4bf956e870
Gustavo Konrad <g.konrad@outlook.com>
2021-09-14 19:05:41 -0300

    added comments

--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,12 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+-- | Takes a string, prepends `Hello, ` and appends `!` to it.
 hello :: String -> String
 hello str = "Hello, " <> str <> "!"
 
+-- | Runs `hello` ad logs it. Comments like this end up in
+-- | documentation generated when you run `spago docs`.
 main :: String -> Effect Unit
 main str = do
   log $ hello str
